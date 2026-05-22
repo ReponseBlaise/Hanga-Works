66f8dc9cf463b6804ad863c31a0496eb158e5ed1
kafurukaleo <kafurukaleo66@gmail.com>
2026-05-22 10:44:32 +0200

    change

--- backend/src/auth/roles.decorator.ts
+++
@@ -1,6 +0,0 @@
-export function Roles(...roles: ('LEARNER' | 'EMPLOYER' | 'INSTITUTION' | 'MENTOR' | 'ADMIN')[]) {
-  return function (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
-    descriptor.value.roles = roles;
-    return descriptor;
-  };
-}
--- backend/src/jobs/dto/update-application-status.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEnum, IsNotEmpty } from 'class-validator';
-import { ApplicationStatus } from '@prisma/client';
-
-export class UpdateApplicationStatusDto {
-  @IsEnum(ApplicationStatus)
-  @IsNotEmpty()
-  status: ApplicationStatus;
-}
